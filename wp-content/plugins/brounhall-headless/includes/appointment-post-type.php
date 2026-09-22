cde9f1097f0ec1d3736d37a1fb42bf6c6c424370
Sanu Khan <sanu.khan@techcarrot.ae>
2026-09-22 15:20:38 +0400

    fix: restore appointment admin menu access

--- wp-content/plugins/brounhall-headless/includes/appointment-post-type.php
+++ wp-content/plugins/brounhall-headless/includes/appointment-post-type.php
@@ -3,13 +3,18 @@ defined( 'ABSPATH' ) || exit;
 
 add_action( 'init', 'brounhall_register_appointment_post_type' );
 add_action( 'admin_menu', function () {
-	if ( current_user_can( 'manage_options' ) ) {
-		add_menu_page( __( 'Appointments', 'brounhall-headless' ), __( 'Appointments', 'brounhall-headless' ), 'manage_options', 'edit.php?post_type=bh_appointment', '', 'dashicons-calendar-alt', 26 );
+	if ( current_user_can( 'edit_appointments' ) ) {
+		add_menu_page( __( 'Appointments', 'brounhall-headless' ), __( 'Appointments', 'brounhall-headless' ), 'edit_appointments', 'edit.php?post_type=bh_appointment', '', 'dashicons-calendar-alt', 26 );
 	}
 }, 30 );
 add_action( 'init', function () {
 	$role = get_role( 'administrator' );
-	if ( $role ) { $role->add_cap( 'brounhall_view_appointment_pii' ); }
+	if ( $role ) {
+		foreach ( array( 'edit_appointment', 'read_appointment', 'delete_appointment', 'edit_appointments', 'edit_others_appointments', 'publish_appointments', 'read_private_appointments', 'delete_appointments', 'delete_private_appointments', 'delete_published_appointments', 'delete_others_appointments' ) as $capability ) {
+			$role->add_cap( $capability );
+		}
+		$role->add_cap( 'brounhall_view_appointment_pii' );
+	}
 }, 20 );
 
 function brounhall_register_appointment_post_type() {
@@ -23,13 +28,7 @@ function brounhall_register_appointment_post_type() {
 		'rewrite' => false,
 		'query_var' => false,
 		'supports' => array( 'title' ),
-		'capability_type' => 'appointment',
-		'capabilities' => array(
-			'edit_post' => 'manage_options', 'read_post' => 'manage_options', 'delete_post' => 'manage_options',
-			'edit_posts' => 'manage_options', 'edit_others_posts' => 'manage_options', 'publish_posts' => 'manage_options',
-			'read_private_posts' => 'manage_options', 'delete_posts' => 'manage_options', 'delete_private_posts' => 'manage_options',
-			'delete_published_posts' => 'manage_options', 'delete_others_posts' => 'manage_options',
-		),
+		'capability_type' => array( 'appointment', 'appointments' ),
 		'map_meta_cap' => true,
 	) );
 }
